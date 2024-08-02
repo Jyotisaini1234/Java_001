0ad5c9918041f73df5de28ecf461f6fa63f8edd1
Jyotisaini1234 <jyoti.saini@surakshitnivesh.com>
2024-08-02 23:11:11 +0530

    learn about function in java

---
+++ function/function.java
@@ -0,0 +1,34 @@
+package Java_001.function;
+import java.util.*;
+public class function {
+//     public static int calculateProduct(int a ,int b) {
+//       return a*b;
+//         }
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//     int  a= sc.nextInt();
+//     int  b= sc.nextInt();
+// System.out.println("Product of a and b is "+calculateProduct(a, b));
+public static void calculateFactorials(int n ){
+        //////////Loop
+
+        if(n < 0){
+            System.out.println("Invalid Number");
+        return;
+        }
+int factorial = 1;
+for(int i =n; i>1;i--){
+factorial = factorial*i;
+}
+System.out.println(factorial);
+return;
+}    
+
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n =sc.nextInt();
+    calculateFactorials(n);
+}
+
+
+}
